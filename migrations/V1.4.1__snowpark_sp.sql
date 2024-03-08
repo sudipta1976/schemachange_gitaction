709ffacd684ca0afde98101446fa506180c618c3
Sudipta SARKAR <ssarkar@mdsol.com>
2024-03-08 16:19:49 -0500

    snowpark

--- migrations/V1.4.1__snowpark_sp.sql
+++ migrations/V1.4.1__snowpark_sp.sql
@@ -8,11 +8,10 @@ comment = 'snowflake as sandbox for snowpark'
 EXECUTE AS CALLER
 AS '
 import snowflake.snowpark as snowpark
-from snowflake.snowpark.functions import col
 import snowflake.snowpark.functions as f
 
 def main(session: snowpark.Session):
-    df = session.table('DEMO.DT_DEMO.CUST_INFO')
-    agg_df = df.agg(f.min('CUSTid').alias('TEST'),f.max('custid').as_("TEST2"))
+    df = session.table("DEMO.DT_DEMO.CUST_INFO")
+    agg_df = df.agg(f.min("CUSTid").alias("TEST"),f.max("custid").as_("TEST2"))
     return df
 '
